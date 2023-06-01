f09df1b8e2932b2597d768e40602cfaa1fdcca68
Joe Baldwin <124543001+JoeB007@users.noreply.github.com>
2023-06-01 18:15:41 -0500

    states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
