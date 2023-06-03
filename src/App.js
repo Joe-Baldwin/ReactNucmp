5a49e651e4d2ad5b92e876704efd0b7cee00dd01
Joe Baldwin <124543001+JoeB007@users.noreply.github.com>
2023-06-03 12:00:15 -0500

    Partners

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -16,6 +17,7 @@ function App() {
                 <Route path='contact' element={<ContactPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
                 <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+                <Route path='about' element={<AboutPage />} />
             </Routes>
             <Footer />
         </div>
