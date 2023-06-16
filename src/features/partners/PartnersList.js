b5c5f43edc0eac798aa13e49f2b431a833023686
Joe Baldwin <124543001+JoeB007@users.noreply.github.com>
2023-06-16 16:05:56 -0500

    redux

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,9 +1,10 @@
 import Partner from "./Partner";
 import { selectAllPartners } from "./partnersSlice";
 import {Col} from 'reactstrap';
+import { useSelector } from 'react-redux';
 
 const PartnersList = () => {
-    const partners = selectAllPartners();
+    const partners = useSelector(selectAllPartners);
     return (
         <Col className="mt-4">
             {partners.map((partner) => {
