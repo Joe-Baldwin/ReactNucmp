6dcc71a7512018b40d30c06f2ca8d01c065bbec0
Joe Baldwin <124543001+JoeB007@users.noreply.github.com>
2023-06-23 02:55:33 -0500

    rest of json-server asyncs

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,8 +1,9 @@
 import { Col, Row } from 'reactstrap';
-
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
-import { selectFeaturedPartners } from '../partners/partnersSlice';
+import { selectFeaturedPartner } from '../partners/partnersSlice';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { useSelector } from 'react-redux';
 
@@ -10,17 +11,24 @@ const DisplayList = () => {
     const items = useSelector((state) => [
         selectFeaturedCampsite(state),
         selectFeaturedPromotion(state),
-        selectFeaturedPartners(state)
+        selectFeaturedPartner(state)
     ]);
 
     console.log('display items:', items);
     return (
         <Row>
             {items.map((item, idx) => {
+                const { featuredItem, isLoading, errMsg } = item;
+                if (isLoading) {
+                    return <Loading key={idx} />;
+                }
+                if (errMsg) {
+                    return <Error errMsg={errMsg} key={idx} />;
+                }
                 return (
-                    item && (
+                    featuredItem && (
                         <Col md className='m-1' key={idx}>
-                            <AnimatedDisplayCard item={item} />
+                            <AnimatedDisplayCard item={featuredItem} />
                         </Col>
                     )
                 );
