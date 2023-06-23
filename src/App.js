3384d70d23eac8fa55d3d9f6e5cb18b34f731e10
Joe Baldwin <124543001+JoeB007@users.noreply.github.com>
2023-06-23 02:41:36 -0500

    json-server-async partners

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
+import { fetchPartners } from './features/partners/partnersSlice';
 
 function App() {
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchCampsites());
+        dispatch(fetchPartners());
     }, [dispatch]);
 
 
